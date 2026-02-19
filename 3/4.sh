6dc65bffd4419668f9f91c69e31208c43f6e5eae
devopsrustem <rusremu@gmail.com>
2026-02-19 16:46:23 +0300

    Update 4.sh

--- 3/4.sh
+++ 3/4.sh
@@ -549,3 +549,34 @@ local 10.99.91.49 from 10.99.91.49 dev lo uid 12002
 10.99.91.49 from 10.99.91.33 via 10.99.91.32 dev ens3np0 uid 12004 
     cache 
 
+
+export MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.39
+
+NCCL_SOCKET_IFNAME=bond0 GLOO_SOCKET_IFNAME=ens108np0 \
+NCCL_DEBUG=WARN NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=0 \
+NCCL_IB_HCA=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11 \
+NCCL_CROSS_NIC=1 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.39 \
+  --port 30000 \
+  --dist-init-addr 10.99.91.39:5000 \
+  --nnodes 2 \
+  --node-rank 0 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode prefill \
+  --disaggregation-ib-device mlx5_0
+
+  export MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.41
+# ... остальные параметры
+
+
+# На каждой ноде проверь:
+python3 -c "import socket; print(socket.gethostbyname(socket.gethostname()))"
