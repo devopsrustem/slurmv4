01f1134ea690f3361881524323830f8427700ed7
devopsrustem <rusremu@gmail.com>
2026-02-19 11:52:07 +0300

    Update 4.sh

--- 3/4.sh
+++ 3/4.sh
@@ -47,3 +47,110 @@ curl -v http://10.99.91.33:30001/health
 
 launch_server.py: error: unrecognized arguments: --disaggregation-decode-url http://10.99.91.49:30001,http://10.99.91.33:30001
 [W219 11:47:05.252714350 AllocatorConfig.cpp:28] Warning: PYTORCH_CUDA_ALLOC_CONF is deprecated, use PYTORCH_ALLOC_CONF instead (function operator())
+
+
+NCCL_SOCKET_IFNAME=bond0 NCCL_DEBUG=WARN NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=1 NCCL_IB_HCA=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11 \
+NCCL_CROSS_NIC=1 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.39 \
+  --port 30000 \
+  --dist-init-addr 10.82.101.193:5000 \
+  --nnodes 2 \
+  --node-rank 0 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode prefill \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
+
+2я
+NCCL_SOCKET_IFNAME=bond0 NCCL_DEBUG=WARN NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=1 NCCL_IB_HCA=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11 \
+NCCL_CROSS_NIC=1 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.41 \
+  --port 30000 \
+  --dist-init-addr 10.82.101.193:5000 \
+  --nnodes 2 \
+  --node-rank 1 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode prefill \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
+3я
+NCCL_SOCKET_IFNAME=bond0 NCCL_DEBUG=WARN NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=1 NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7 \
+NCCL_CROSS_NIC=1 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.49 \
+  --port 30001 \
+  --dist-init-addr 10.82.101.249:5000 \
+  --nnodes 2 \
+  --node-rank 0 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode decode \
+  --max-running-requests 128 \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
+
+  4я
+
+  NCCL_SOCKET_IFNAME=bond0 NCCL_DEBUG=WARN NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=1 NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7 \
+NCCL_CROSS_NIC=1 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.33 \
+  --port 30001 \
+  --dist-init-addr 10.82.101.249:5000 \
+  --nnodes 2 \
+  --node-rank 1 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode decode \
+  --max-running-requests 128 \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
+
+
+  curl http://10.99.91.39:30000/health  # prefill 1
+curl http://10.99.91.41:30000/health  # prefill 2
+curl http://10.99.91.49:30001/health  # decode 3
+curl http://10.99.91.33:30001/health  # decode 4
+
+# Все должны вернуть HTTP 200 OK
+
+# Потом запрос через router:
+curl -X POST http://10.82.101.193:8000/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "/app/models/Deepseek-R1/",
+    "messages": [{"role": "user", "content": "Hello"}],
+    "max_tokens": 10
+  }'
