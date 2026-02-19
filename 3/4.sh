3da4cda094776977b319517a600d9ccbb288e16e
devopsrustem <rusremu@gmail.com>
2026-02-19 13:31:30 +0300

    Update 4.sh

--- 3/4.sh
+++ 3/4.sh
@@ -342,3 +342,33 @@ TOKENIZERS_PARALLELISM=false \
   --max-running-requests 128 \
   --disaggregation-ib-device mlx5_0 \
   --disaggregation-bootstrap-port 8998
+
+
+  1
+
+  NCCL_SOCKET_IFNAME=bond0 GLOO_SOCKET_IFNAME=ens108np0 \
+NCCL_DEBUG=WARN NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=0 \
+NCCL_IB_HCA=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11 \
+NCCL_CROSS_NIC=1 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.39 \
+  --port 30000 \
+  --dist-init-addr 10.99.91.39:5000 \
+  --nnodes 2 \
+  --node-rank 0 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode prefill \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
+
+  
+SGLANG_KV_TRANSFER_USE_TCP=1
+sha256sum /opt/mooncake-libs/run-with-new-libstdc.sh
+ls -lh /opt/mooncake-libs/
