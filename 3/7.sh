f719d7ca7c76b9b03bbecf64096a08f35036e713
devopsrustem <rusremu@gmail.com>
2026-02-25 19:30:54 +0300

    Update 7.sh

--- 3/7.sh
+++ 3/7.sh
@@ -207,3 +207,168 @@ rm -rf /tmp/torch_* /tmp/nccl_* /dev/shm/torch_* /tmp/mooncake* ~/.mooncake 2>/d
 # Проверь что ничего не осталось:
 ps aux | grep -E "(sglang|python.*sglang)" | grep -v grep
 ss -tlnp | grep -E "(5000|30000|30001|8998)"
+
+
+3
+
+pkill -9 -f sglang
+sleep 2
+
+MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.49 \
+GLOO_SOCKET_FAMILY=AF_INET \
+NCCL_SOCKET_IFNAME=ens3np0 \
+NCCL_TIMEOUT=600 \
+MASTER_ADDR=10.99.91.49 \
+MASTER_PORT=5000 \
+GLOO_SOCKET_IFNAME=ens3np0 \
+NCCL_DEBUG=WARN \
+NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX \
+NCCL_IB_GID_INDEX=3 \
+NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=0 \
+NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7 \
+NCCL_CROSS_NIC=1 \
+CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.49 \
+  --port 30001 \
+  --dist-init-addr 10.99.91.49:5000 \
+  --nnodes 2 \
+  --node-rank 0 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode decode \
+  --max-running-requests 128 \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
+
+  6
+
+
+  pkill -9 -f sglang
+sleep 2
+
+MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.35 \
+GLOO_SOCKET_FAMILY=AF_INET \
+NCCL_SOCKET_IFNAME=ens3np0 \
+NCCL_TIMEOUT=600 \
+MASTER_ADDR=10.99.91.49 \
+MASTER_PORT=5000 \
+GLOO_SOCKET_IFNAME=ens3np0 \
+NCCL_DEBUG=WARN \
+NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX \
+NCCL_IB_GID_INDEX=3 \
+NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=0 \
+NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7 \
+NCCL_CROSS_NIC=1 \
+CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.35 \
+  --port 30001 \
+  --dist-init-addr 10.99.91.49:5000 \
+  --nnodes 2 \
+  --node-rank 1 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode decode \
+  --max-running-requests 128 \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
+
+
+
+  1
+
+
+pkill -9 -f sglang
+sleep 2
+
+MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.39 \
+GLOO_SOCKET_FAMILY=AF_INET \
+NCCL_SOCKET_IFNAME=ens108np0 \
+NCCL_TIMEOUT=600 \
+MASTER_ADDR=10.99.91.39 \
+MASTER_PORT=5000 \
+GLOO_SOCKET_IFNAME=ens108np0 \
+NCCL_DEBUG=WARN \
+NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX \
+NCCL_IB_GID_INDEX=3 \
+NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=0 \
+NCCL_IB_HCA=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11 \
+NCCL_CROSS_NIC=1 \
+CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.39 \
+  --port 30000 \
+  --dist-init-addr 10.99.91.39:5000 \
+  --nnodes 2 \
+  --node-rank 0 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode prefill \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998 \
+  --disable-cuda-graph
+
+
+  2
+
+
+
+pkill -9 -f sglang
+sleep 2
+
+MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.41 \
+GLOO_SOCKET_FAMILY=AF_INET \
+NCCL_SOCKET_IFNAME=ens108np0 \
+NCCL_TIMEOUT=600 \
+MASTER_ADDR=10.99.91.39 \
+MASTER_PORT=5000 \
+GLOO_SOCKET_IFNAME=ens108np0 \
+NCCL_DEBUG=WARN \
+NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX \
+NCCL_IB_GID_INDEX=3 \
+NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=0 \
+NCCL_IB_HCA=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11 \
+NCCL_CROSS_NIC=1 \
+CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.41 \
+  --port 30000 \
+  --dist-init-addr 10.99.91.39:5000 \
+  --nnodes 2 \
+  --node-rank 1 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode prefill \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998 \
+  --disable-cuda-graph
+  
