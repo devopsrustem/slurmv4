7dab41c35611f22f3ac08fac2a7e7c5c5f712fe0
devopsrustem <rusremu@gmail.com>
2026-02-25 16:39:28 +0300

    Update 5.sh

--- 3/5.sh
+++ 3/5.sh
@@ -661,3 +661,37 @@ RuntimeError: NCCL error: remote process exited or there was a network error
 
 [2026-02-25 16:33:59] Received sigquit from a child process. It usually means the child failed.
 Killed
+
+
+
+export MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.49
+export GLOO_SOCKET_FAMILY=AF_INET
+
+NCCL_SOCKET_IFNAME=bond0 \
+GLOO_SOCKET_IFNAME=ens3np0 \
+NCCL_DEBUG=WARN \
+NCCL_IB_DISABLE=0 \
+NCCL_NET_GDR_LEVEL=PIX \
+NCCL_IB_GID_INDEX=3 \
+NCCL_IB_TC=106 \
+NCCL_NVLS_ENABLE=0 \
+NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7 \
+NCCL_CROSS_NIC=1 \
+CUDA_DEVICE_MAX_CONNECTIONS=1 \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb=256 \
+TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh \
+/app/sglang/sglang-latest/bin/python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ \
+  --trust-remote-code \
+  --tp-size 16 \
+  --host 10.99.91.49 \
+  --port 30001 \
+  --dist-init-addr 10.99.91.49:5000 \
+  --nnodes 2 \
+  --node-rank 0 \
+  --mem-fraction-static 0.8 \
+  --disaggregation-mode decode \
+  --max-running-requests 128 \
+  --disaggregation-ib-device mlx5_0 \
+  --disaggregation-bootstrap-port 8998
