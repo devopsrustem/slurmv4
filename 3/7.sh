ec17e9965b4415d1663381de6d6987369593c44f
devopsrustem <rusremu@gmail.com>
2026-02-25 19:21:52 +0300

    Update 7.sh

--- 3/7.sh
+++ 3/7.sh
@@ -170,3 +170,28 @@ TOKENIZERS_PARALLELISM=false \
 
 
 ps aux | grep sglang | grep python3 | head -1 | awk '{print $2}' | xargs -I {} cat /proc/{}/environ | tr '\0' '\n' | grep MOONCAKE
+
+
+[dcbsr_dev@tpgds-aihub0003 ~]$ cat /etc/hosts
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
+#10.82.101.249     tpgds-aihub0003.delta.sbrf.ru tpgds-aihub0003
+10.99.91.49     tpgds-aihub0003.delta.sbrf.ru tpgds-aihub0003
+[dcbsr_dev@tpgds-aihub0003 ~]$ python3 -c "import socket; print(socket.gethostbyname(socket.gethostname()))"
+10.99.91.49
+[dcbsr_dev@tpgds-aihub0003 ~]$ ps aux | grep sglang | grep python3 | head -1 | awk '{print $2}' | xargs -I {} cat /proc/{}/environ | tr '\0' '\n' | grep MOONCAKE
+MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.49
+
+
+((sglang-0.5.8.post1) ) [dcbsr_dev@tpgds-aihub0006 ~]$ [dcbsr_dev@tpgds-aihub0003 ~]$ ps aux | grep sglang | grep python3 | head -1 | awk '{print $2}' | xargs -I {} cat /proc/{}/environ | tr '\0' '\n' | grep MOONCAKE
+MOONCAKE_TRANSFER_ENGINE_IP=10.99.91.49
+-bash: [dcbsr_dev@tpgds-aihub0003: command not found
+((sglang-0.5.8.post1) ) [dcbsr_dev@tpgds-aihub0006 ~]$ cat /etc/hosts
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
+
+
+10.99.91.35     tpgds-aihub0006.delta.sbrf.ru tpgds-aihub0006
+((sglang-0.5.8.post1) ) [dcbsr_dev@tpgds-aihub0006 ~]$ python3 -c "import socket; print(socket.gethostbyname(socket.gethostname()))"
+10.99.91.35
+
