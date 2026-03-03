d4e0011a3aa365e9677834b5f5aa48a0a1409de7
devopsrustem <rusremu@gmail.com>
2026-03-03 12:16:49 +0300

    Update 13.sh

--- 3/13.sh
+++ 3/13.sh
@@ -117,3 +117,39 @@ tpgds-aihub0007:185309:185309 [7] NCCL INFO misc/utils.cc:233 memory stack hunk 
 tpgds-aihub0007:185303:186751 [1] NCCL INFO transport/net.cc:1227 -> 6
 
 на этом зависает
+
+
+0007:
+bashSGLANG_HOST_IP=10.99.91.59 MC_GID_INDEX=3 MC_TCP_BIND_ADDRESS=10.99.91.59 \
+SGLANG_LOCAL_IP_NIC=enp25s0np0 GLOO_SOCKET_FAMILY=AF_INET GLOO_SOCKET_IFNAME=enp25s0np0 \
+NCCL_SOCKET_IFNAME=enp25s0np0 NCCL_IB_HCA=mlx5_0 \
+NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 NCCL_IB_DISABLE=0 NCCL_NET_GDR_LEVEL=PIX \
+NCCL_NVLS_ENABLE=0 NCCL_CROSS_NIC=1 NCCL_TIMEOUT=600 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+NCCL_TREE_THRESHOLD=0 NCCL_ALGO=Ring NCCL_PROTO=Simple \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ --trust-remote-code \
+  --tp-size 16 --host 10.99.91.59 --port 30001 \
+  --dist-init-addr 10.99.91.59:5000 --nnodes 2 --node-rank 0 \
+  --mem-fraction-static 0.8 --disaggregation-mode decode \
+  --disaggregation-transfer-backend nixl \
+  --disaggregation-ib-device mlx5_0,mlx5_1,mlx5_2,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_11 \
+  --max-running-requests 128
+0008:
+bashSGLANG_HOST_IP=10.99.91.63 MC_GID_INDEX=3 MC_TCP_BIND_ADDRESS=10.99.91.63 \
+SGLANG_LOCAL_IP_NIC=enp25s0np0 GLOO_SOCKET_FAMILY=AF_INET GLOO_SOCKET_IFNAME=enp25s0np0 \
+NCCL_SOCKET_IFNAME=enp25s0np0 NCCL_IB_HCA=mlx5_0 \
+NCCL_IB_GID_INDEX=3 NCCL_IB_TC=106 NCCL_IB_DISABLE=0 NCCL_NET_GDR_LEVEL=PIX \
+NCCL_NVLS_ENABLE=0 NCCL_CROSS_NIC=1 NCCL_TIMEOUT=600 CUDA_DEVICE_MAX_CONNECTIONS=1 \
+NCCL_TREE_THRESHOLD=0 NCCL_ALGO=Ring NCCL_PROTO=Simple \
+PYTORCH_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256 TOKENIZERS_PARALLELISM=false \
+/opt/mooncake-libs/run-with-new-libstdc.sh python3 -m sglang.launch_server \
+  --model-path /app/models/Deepseek-R1/ --trust-remote-code \
+  --tp-size 16 --host 10.99.91.63 --port 30001 \
+  --dist-init-addr 10.99.91.59:5000 --nnodes 2 --node-rank 1 \
+  --mem-fraction-static 0.8 --disaggregation-mode decode \
+  --disaggregation-transfer-backend nixl \
+  --disaggregation-ib-device mlx5_0,mlx5_1,mlx5_2,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_11 \
+  --max-running-requests 128
+
+  
