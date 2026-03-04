11de37b27a915626ccf2da88562e9367941ec64a
devopsrustem <rusremu@gmail.com>
2026-03-04 12:18:17 +0300

    Update 14.sh

--- 3/14.sh
+++ 3/14.sh
@@ -36,3 +36,93 @@ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 \
   --master_addr=10.99.91.59 --master_port=29500 \
   /tmp/test_nccl.py
 Если зависнет — проблема именно в 0007↔0008 inter-node NCCL.
+
+((sglang-0.5.9) ) [dcbsr_dev@tpgds-aihub0008 ~]$ NCCL_NVLS_ENABLE=0 NCCL_SOCKET_IFNAME=enp25s0np0 NCCL_IB_HCA=mlx5_0 NCCL_IB_GID_INDEX=3 NCCL_TREE_THRESHOLD=0 NCCL_ALGO=Ring torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1   --master_addr=10.99.91.59 --master_port=29500   /tmp/test_nccl.py
+W0304 12:17:15.750000 469293 torch/distributed/run.py:803] 
+W0304 12:17:15.750000 469293 torch/distributed/run.py:803] *****************************************
+W0304 12:17:15.750000 469293 torch/distributed/run.py:803] Setting OMP_NUM_THREADS environment variable for each process to be 1 in default, to avoid your system being overloaded, please further tune the variable for optimal performance in your application as needed. 
+W0304 12:17:15.750000 469293 torch/distributed/run.py:803] *****************************************
+[W304 12:17:18.228653728 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.223723147 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.223868351 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.225036144 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.225098990 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.226303063 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.233063946 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.269516582 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.283025107 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [::ffff:10.99.91.59]:29500 (errno: 97 - Address family not supported by protocol).
+[rank12]: Traceback (most recent call last):
+[rank12]:   File "/tmp/test_nccl.py", line 6, in <module>
+[rank12]:     x = torch.ones(1000).cuda(rank)
+[rank12]:         ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+[rank12]: torch.AcceleratorError: CUDA error: invalid device ordinal
+[rank12]: GPU device may be out of range, do you have enough GPUs?
+[rank12]: CUDA kernel errors might be asynchronously reported at some other API call, so the stacktrace below might be incorrect.
+[rank12]: For debugging consider passing CUDA_LAUNCH_BLOCKING=1
+[rank12]: Compile with `TORCH_USE_CUDA_DSA` to enable device-side assertions.
+
+[rank11]: Traceback (most recent call last):
+[rank11]:   File "/tmp/test_nccl.py", line 6, in <module>
+[rank11]:     x = torch.ones(1000).cuda(rank)
+[rank11]:         ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+[rank11]: torch.AcceleratorError: CUDA error: invalid device ordinal
+[rank11]: GPU device may be out of range, do you have enough GPUs?
+[rank11]: CUDA kernel errors might be asynchronously reported at some other API call, so the stacktrace below might be incorrect.
+[rank11]: For debugging consider passing CUDA_LAUNCH_BLOCKING=1
+[rank11]: Compile with `TORCH_USE_CUDA_DSA` to enable device-side assertions.
+
+W0304 12:17:22.059000 469293 torch/distributed/elastic/multiprocessing/api.py:908] Sending process 469398 closing signal SIGTERM
+W0304 12:17:22.060000 469293 torch/distributed/elastic/multiprocessing/api.py:908] Sending process 469399 closing signal SIGTERM
+W0304 12:17:22.060000 469293 torch/distributed/elastic/multiprocessing/api.py:908] Sending process 469400 closing signal SIGTERM
+W0304 12:17:22.061000 469293 torch/distributed/elastic/multiprocessing/api.py:908] Sending process 469403 closing signal SIGTERM
+W0304 12:17:22.061000 469293 torch/distributed/elastic/multiprocessing/api.py:908] Sending process 469404 closing signal SIGTERM
+W0304 12:17:22.062000 469293 torch/distributed/elastic/multiprocessing/api.py:908] Sending process 469405 closing signal SIGTERM
+E0304 12:17:22.232000 469293 torch/distributed/elastic/multiprocessing/api.py:882] failed (exitcode: 1) local_rank: 4 (pid: 469402) of binary: /app/sglang/sglang-0.5.9/bin/python3.12
+Traceback (most recent call last):
+  File "/app/sglang/sglang-0.5.9/bin/torchrun", line 6, in <module>
+    sys.exit(main())
+             ^^^^^^
+  File "/app/sglang/sglang-0.5.9/lib64/python3.12/site-packages/torch/distributed/elastic/multiprocessing/errors/__init__.py", line 357, in wrapper
+    return f(*args, **kwargs)
+           ^^^^^^^^^^^^^^^^^^
+  File "/app/sglang/sglang-0.5.9/lib64/python3.12/site-packages/torch/distributed/run.py", line 936, in main
+    run(args)
+  File "/app/sglang/sglang-0.5.9/lib64/python3.12/site-packages/torch/distributed/run.py", line 927, in run
+    elastic_launch(
+  File "/app/sglang/sglang-0.5.9/lib64/python3.12/site-packages/torch/distributed/launcher/api.py", line 156, in __call__
+    return launch_agent(self._config, self._entrypoint, list(args))
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/app/sglang/sglang-0.5.9/lib64/python3.12/site-packages/torch/distributed/launcher/api.py", line 293, in launch_agent
+    raise ChildFailedError(
+torch.distributed.elastic.multiprocessing.errors.ChildFailedError: 
+============================================================
+/tmp/test_nccl.py FAILED
+------------------------------------------------------------
+Failures:
+  <NO_OTHER_FAILURES>
+------------------------------------------------------------
+Root Cause (first observed failure):
+[0]:
+  time      : 2026-03-04_12:17:22
+  host      : tpgds-aihub0008.delta.sbrf.ru
+  rank      : 12 (local_rank: 4)
+  exitcode  : 1 (pid: 469402)
+  error_file: <N/A>
+  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
+============================================================
+((sglang-0.5.9) ) [dcbsr_dev@tpgds-aihub0008 ~]$ 
+
+((sglang-0.5.9) ) [dcbsr_dev@tpgds-aihub0007 ~]$ NCCL_NVLS_ENABLE=0 NCCL_SOCKET_IFNAME=enp25s0np0 NCCL_IB_HCA=mlx5_0 NCCL_IB_GID_INDEX=3 NCCL_TREE_THRESHOLD=0 NCCL_ALGO=Ring torchrun --nproc_per_node=8 --nnodes=2 --node_rank=0   --master_addr=10.99.91.59 --master_port=29500   /tmp/test_nccl.py
+W0304 12:17:14.161000 267897 torch/distributed/run.py:803] 
+W0304 12:17:14.161000 267897 torch/distributed/run.py:803] *****************************************
+W0304 12:17:14.161000 267897 torch/distributed/run.py:803] Setting OMP_NUM_THREADS environment variable for each process to be 1 in default, to avoid your system being overloaded, please further tune the variable for optimal performance in your application as needed. 
+W0304 12:17:14.161000 267897 torch/distributed/run.py:803] *****************************************
+[W304 12:17:16.381145738 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.890936097 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.890940575 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.891672306 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.891997177 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.938432391 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.941479740 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.942025561 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol).
+[W304 12:17:19.980160588 socket.cpp:767] [c10d] The client socket cannot be initialized to connect to [tpgds-aihub0003.delta.sbrf.ru]:29500 (errno: 97 - Address family not supported by protocol)
