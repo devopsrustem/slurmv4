0e3aa950ba6e889188743400b4839b398cc49d44
devopsrustem <rusremu@gmail.com>
2026-03-04 19:35:34 +0300

    Update 14.sh

--- 3/14.sh
+++ 3/14.sh
@@ -41,3 +41,10 @@ python3 -m sglang.launch_server \
 /app/sglang/sglang-0.5.8.post1/lib/python3.12/site-packages/sglang/srt/distributed/device_communicators/all_reduce_utils.py
 /app/sglang/sglang-0.5.9/lib/python3.12/site-packages/sglang/srt/distributed/device_communicators/__pycache__/all_reduce_utils.cpython-312.pyc
 /app/sglang/sglang-0.5.9/lib/python3.12/site-packages/sglang/srt/distributed/device_communicators/all_reduce_utils.py
+
+
+NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_11 \
+NCCL_IB_GID_INDEX=3 \
+NCCL_SOCKET_IFNAME=enp25s0np0 \
+/opt/nvidia/nvidia_hpc_benchmarks_mpich/cuda13/microbenchmarks/nccl_tests/all_reduce_perf \
+  -b 1M -e 1G -f 2 -g 8
