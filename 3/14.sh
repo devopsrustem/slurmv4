00db9179f73bd27a7233fdeb84f19dea96445f06
devopsrustem <rusremu@gmail.com>
2026-03-05 11:07:17 +0300

    Update 14.sh

--- 3/14.sh
+++ 3/14.sh
@@ -486,3 +486,13 @@ ninja: build stopped: subcommand failed.
 
 
 sudo ln -s /usr/local/cuda-12.8/targets/x86_64-linux/lib /usr/local/cuda-12.8/lib64
+
+
+
+
+7
+
+[dcbsr_dev@tpgds-aihub0007 ~]$ LD_LIBRARY_PATH=/usr/local/cuda-12.8/targets/x86_64-linux/lib:/usr/local/cuda-13.1/lib64:$LD_LIBRARY_PATH NCCL_IB_DISABLE=1 SGLANG_HOST_IP=10.99.91.59 NCCL_SOCKET_IFNAME=enp25s0np0 NCCL_IB_HCA="=mlx5_0,=mlx5_1,=mlx5_2,=mlx5_5,=mlx5_6,=mlx5_7,=mlx5_8,=mlx5_11" NCCL_IB_GID_INDEX=3 python3 -m sglang.launch_server   --model-path /app/models/Deepseek-R1/ --trust-remote-code   --tp-size 16 --host 10.99.91.59 --port 30001   --dist-init-addr 10.99.91.59:5000 --nnodes 2 --node-rank 0   --mem-fraction-static 0.8 --disaggregation-mode decode   --disaggregation-transfer-backend nixl   --disaggregation-ib-device mlx5_0,mlx5_1,mlx5_2,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_11   --max-running-requests 128  --disable-cuda-graph
+8 
+
+[dcbsr_dev@tpgds-aihub0008 ~]$ LD_LIBRARY_PATH=/usr/local/cuda-12.8/targets/x86_64-linux/lib:/usr/local/cuda-13.1/lib64:$LD_LIBRARY_PATH NCCL_IB_DISABLE=1  SGLANG_HOST_IP=10.99.91.63 NCCL_SOCKET_IFNAME=enp25s0np0 NCCL_IB_HCA="=mlx5_0,=mlx5_1,=mlx5_2,=mlx5_5,=mlx5_6,=mlx5_7,=mlx5_8,=mlx5_11" NCCL_IB_GID_INDEX=3 python3 -m sglang.launch_server   --model-path /app/models/Deepseek-R1/ --trust-remote-code   --tp-size 16 --host 10.99.91.63 --port 30001   --dist-init-addr 10.99.91.59:5000 --nnodes 2 --node-rank 1   --mem-fraction-static 0.8 --disaggregation-mode decode   --disaggregation-transfer-backend nixl   --disaggregation-ib-device mlx5_0,mlx5_1,mlx5_2,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_11   --max-running-requests 128 --disable-cuda-graph
